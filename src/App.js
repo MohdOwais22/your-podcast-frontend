8808d7a926095f87ebe56cc8308852d32b1d9fbe
MohdOwais22 <mohdowais762@gmail.com>
2023-04-21 17:21:59 +0530

    project-setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      <h1>hello</h1>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
