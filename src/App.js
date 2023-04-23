4a587e3a69fe27a2d0fa6ad1429a72690f7a29fa
Pranjali <pranjalisharma775@gmail.com>
2023-04-23 13:39:48 +0530

    push login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Signup from './pages/signup/Signup.js';
-// import Login from './pages/login/Login.js';
+// import Signup from './pages/signup/Signup.js';
+import Login from './pages/login/Login.js';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/signup" element={<Signup />} /> */}
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,71 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import './Login.css';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import image from './63163.jpg';
+import LockIcon from '@mui/icons-material/Lock';
 
-const Login = () => {
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <div className="bg">
+        <img src={image} alt="Logo" />
+      </div>
+
+      <div className="container__content">
+        <div className="form__login">
+          <h2>LOGIN</h2>
+          <form className="textbox__login" onSubmit={handleSubmit(onSubmit)}>
+            <div className="login__username">
+              <div className="icons">
+                <AccountCircleIcon />
+              </div>
+              <input
+                spellCheck="false"
+                type="text"
+                placeholder="Username"
+                {...register('username', { required: true })}
+              />
+              {errors.username && (
+                <div className="alert">Username is required</div>
+              )}
+            </div>
+            <div className="login__password">
+              <div className="icon">
+                <LockIcon />
+              </div>
+              <input
+                spellCheck="false"
+                type="password"
+                placeholder="Password"
+                {...register('password', { required: true })}
+              />
+              {errors.password && (
+                <div className="alert">Password is required</div>
+              )}
+            </div>
+            <p>
+              Not logging in ? <a href="#">Sign up here</a>
+            </p>
+            <div id="spinner" className="spinner"></div>
+            <button type="submit" className="submit_btn_login">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
-};
+}
 
 export default Login;
